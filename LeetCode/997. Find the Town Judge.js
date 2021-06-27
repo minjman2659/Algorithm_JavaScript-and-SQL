c5d313bbd2e0fe1f3fb86e877dd662b7cf0559ac
minjman2659 <minjman2659@gmail.com>
2021-06-27 17:52:13 +0900

    LeetCode_997.Find the Town Judge solve updated

--- LeetCode/997. Find the Town Judge.js
+++ LeetCode/997. Find the Town Judge.js
@@ -7,30 +7,50 @@
 // https://leetcode.com/problems/find-the-town-judge/
 
  var findJudge = function(n, trust) {
-    let matrix = new Array(n).fill(0).map((el) => new Array(n).fill(0));
+//     let matrix = new Array(n).fill(0).map((el) => new Array(n).fill(0));
+    
+//     for(let el of trust) {
+//         matrix[el[0]-1][el[1]-1] = 1;
+//     }
+    
+// // ------------------------------------------------------------ matrix 완성
+    
+//     let count = 0;
+//     for(let i=0; i<matrix.length; i++) {
+//         for(let j=0; j<matrix[i].length; j++) {
+//             if(matrix[j][i] === 1) {
+//                 count++;
+//             }
+//         }
+//         if(count === n-1) {
+//             let judge = i;
+//             if(matrix[judge].reduce((sum, cur) => sum + cur) === 0) {
+//                 return judge + 1;
+//             } else {
+//                 return -1;
+//             }
+//         } else {
+//             count = 0;
+//         }
+//     }
+    
+//     return -1;
+
+    let obj = {};
+    let countArr = new Array(n+1).fill(0);
     
     for(let el of trust) {
-        matrix[el[0]-1][el[1]-1] = 1;
+        if(!(el[0] in obj)) {
+            obj[el[0]] = [el[1]];
+        } else {
+            obj[el[0]].push(el[1]);
+        }
+        countArr[el[1]]++;
     }
     
-// ------------------------------------------------------------ matrix 완성
-    
-    let count = 0;
-    for(let i=0; i<matrix.length; i++) {
-        for(let j=0; j<matrix[i].length; j++) {
-            if(matrix[j][i] === 1) {
-                count++;
-            }
-        }
-        if(count === n-1) {
-            let judge = i;
-            if(matrix[judge].reduce((sum, cur) => sum + cur) === 0) {
-                return judge + 1;
-            } else {
-                return -1;
-            }
-        } else {
-            count = 0;
+    for(let i=1; i<countArr.length; i++) {
+        if(countArr[i] === n-1 && obj[i] === undefined) {
+            return i;
         }
     }
     
