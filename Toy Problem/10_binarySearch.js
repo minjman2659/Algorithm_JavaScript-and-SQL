02967f789ad6da8fa45d74bb256c027ce2c36169
minjman2659 <minjman2659@gmail.com>
2021-06-28 09:26:24 +0900

    Toy Problem_10_binarySearch Solved

---
+++ Toy Problem/10_binarySearch.js
@@ -0,0 +1,28 @@
+// https://urclass.codestates.com/codeproblem/b06d36ed-129a-44e5-a624-90a8768848ad
+
+const binarySearch = function (arr, target) {
+    let high = arr.length-1;
+    let low = 0;
+  
+    while(arr[high] > arr[low]) {
+      let mid = parseInt((high + low) / 2);
+  
+      if(target === arr[high]) {
+        return high;
+      }
+      if(target === arr[low]) {
+        return low;
+      }
+      if(target === arr[mid]) {
+        return mid;
+      }
+  
+      if(target > arr[mid]) {
+        low = mid + 1;
+      } else if(target < arr[mid]) {
+        high = mid - 1;
+      }
+    }
+  
+    return -1;
+  };
