6799ba263efbfe12cb7e247fcd2e30b78bc1f678
minjman2659 <minjman2659@gmail.com>
2021-07-01 09:57:39 +0900

    Recent Updated

--- Toy Problem/04_bubbleSort.js
+++ Toy Problem/04_bubbleSort.js
@@ -1,21 +1,23 @@
 // https://urclass.codestates.com/codeproblem/59f6934a-fe46-4dbe-956a-d1d21d12b797
 
 const bubbleSort = function (arr) {
-    for(let i=0; i<arr.length; i++) {
-      let swap = false;
-      for(let j=1; j<arr.length-i; j++) {
-        if(arr[j-1] > arr[j]) {
-          let small = arr[j];
-          arr[j] = arr[j-1];
-          arr[j-1] = small;
-          swap = true;
-        }
-      }
-      if(swap === false) {
-        break;
+  // 버블정렬은 맨 뒤의 요소를 가장 큰 요소로 만들어 해당 요소 앞까지만 계속 반복 비교하는 방법
+
+  for(let i=0; i<arr.length; i++) {
+    let swap = false;
+    for(let j=1; j<arr.length-i; j++) {
+      if(arr[j-1] > arr[j]) {
+        let small = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = small;
+        swap = true;
       }
     }
-  
-    return arr;
-  };
+    if(swap === false) {
+      break;
+    }
+  }
+
+  return arr;
+};
   
---
+++ Toy Problem/13_insertionSort.js
@@ -0,0 +1,17 @@
+// https://urclass.codestates.com/codeproblem/9347b40b-4775-4ab9-92fb-63bbed16a14f
+
+const insertionSort = function (arr, callback = (val) => val) {
+    // 삽입정렬은 뚝 뚝 잘라가면서 대소비교를 통해 정렬하는 방법
+  
+    for(let i=1; i<arr.length; i++) {
+      for(let j=i-1; j>=0; j--) {
+        if(callback(arr[j+1]) < callback(arr[j])) {
+          let small = arr[j+1];
+          arr[j+1] = arr[j];
+          arr[j] = small;
+        }
+      } 
+    }
+  
+    return arr;
+  };
