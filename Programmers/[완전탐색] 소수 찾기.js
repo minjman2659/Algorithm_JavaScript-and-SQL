95a1112760a743d4ef71f0af639de191ad7bf459
minjman2659 <minjman2659@gmail.com>
2021-07-14 16:28:58 +0900

    Programmers_[완전탐색]소수 찾기 Solved

--- Programmers/[완전탐색] 소수 찾기.js
+++ Programmers/[완전탐색] 소수 찾기.js
@@ -1,13 +1,8 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42839#qna
 
-// 순열 문제인 것 같다... 아직 순열을 모르기에 공부 후 다시 풀자
-// Unsolved yet
-
 function solution(numbers) {
     // 먼저 요소들의 부분 조합들을 배열로 구한 후, 소수의 개수를 구하자!
-    
     numbers = numbers.split('');
-    numbers.sort((a, b) => Number(a) - Number(b));
     
     // 소수 판별 모듈
     const isPrime = (num) => {
@@ -26,42 +21,41 @@ function solution(numbers) {
         return true;
     }
     
-    const result = [];
-    const check = new Array(numbers.length).fill(false);
-    
-    function arrPush(arr) {
-        let tmp = '';
-        for(let i=0; i<arr.length; i++) {
-            if(check[i]) {
-                tmp = tmp + arr[i];
-            }
-        }
-        result.push(tmp);
+    // 순열 모듈
+    let result = [];
+    for(let i=1; i<=numbers.length; i++) {
+        result.push(...getPermutation(numbers, i));
     }
     
-    function checkFunc(start) {
-        for(let j=start; j<numbers.length; j++) {
-            check[j] = true;
-            arrPush(numbers);
-            checkFunc(j+1);
-            check[j] = false;
+    function getPermutation(arr, num) {
+        let tmp = [];
+        if(num === 1) {
+            return arr.map((el) => [el]);
         }
+
+        arr.forEach((fixed, idx, origin) => {
+            let rest = [...origin.slice(0, idx), ...origin.slice(idx + 1)];
+            let combinations = getPermutation(rest, num - 1);
+            let attached = combinations.map(el => [fixed, ...el]);
+            tmp.push(...attached);
+        }) 
+        return tmp;
     }
     
-    checkFunc(0);
-    const makeNumArr = result.map((el) => Number(el));
-    let obj = {};
-    for(let numArr of makeNumArr) {
-        if(!(numArr in obj)) {
-            obj[numArr] = true;
+    // 중복 체크
+    result = result.map((el) => el.join(''));
+    const obj = {};
+    for(let el of result) {
+        if(!(Number(el) in obj)) {
+            obj[el] = true;
         }
     }
+    const answer = Object.keys(obj).map((el) => Number(el));
     
-    let realNumArr = Object.keys(obj);
+    // 소수 판별 카운트
     let count = 0;
-    
-    for(let num of realNumArr) {
-        if(isPrime(num)) {
+    for(let final of answer) {
+        if(isPrime(final)) {
             count++;
         }
     }
