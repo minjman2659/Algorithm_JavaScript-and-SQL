9de66036bef043fb36d8b4ad021764302ff0830c
minjman2659 <minjman2659@gmail.com>
2021-07-21 13:02:55 +0900

    Recent Updated

--- Toy Problem/(Advanced) [DP] 금고를 털어라.js
+++ Toy Problem/(Advanced) [DP] 금고를 털어라.js
@@ -9,7 +9,7 @@ function ocean(target, type) {
     bag[0] = 1; 
   
     for(let i = 0; i < type.length; i++) { 
-      for(let j = 1; j <= target; j++){   // 0번째 인덱스를 제외하고
+      for(let j = 1; j <= bag.length; j++){   // 0번째 인덱스를 제외하고
         if(type[i] <= j) {    // 주어진 type 배열의 요소들을 가지고 만들 수 있는 모든 수에 값을 할당하기 위한 조건
           bag[j] = bag[j] + bag[j-type[i]];   // 주어진 type 배열의 요소들을 가지고 만들지 못하는 값들은 전부 0이 된다.
         }
