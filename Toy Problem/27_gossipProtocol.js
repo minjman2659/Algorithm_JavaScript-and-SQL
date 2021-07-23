5a7fa263fc683409afa128187e32dc5f384d076e
minjman2659 <minjman2659@gmail.com>
2021-07-23 18:37:31 +0900

    Toy Problem_27_gossipProtocol Solved

--- Toy Problem/27_gossipProtocol.js
+++ Toy Problem/27_gossipProtocol.js
@@ -12,41 +12,41 @@ const createMatrix = (village) => {
     });
     return matrix;
   };
+// ㄴ createMatrix는 주어진 함수_행렬(Matrix)를 만들어주는 모듈  
+
+const gossipProtocol = function (village, row, col) {
+  const matrix = createMatrix(village);
+  let count = 0;
+  let start = [row, col, count];
   
-  const gossipProtocol = function (village, row, col) {
-    const matrix = createMatrix(village);
-    let count = 0;
-    let start = [row, col, count];
-    
-    return bfs(start);
+  return bfs(start);
+
   
-    
-    function bfs(location) {
-      const queue = [location];
-      const countSaver = [];
-      matrix[location[0]][location[1]] = 1;
-  
-      while(queue.length > 0) {
-        let [Y, X, count] = queue.shift();
-  
-        if(Y < 0 || X < 0 || Y >= matrix.length || X >= matrix[0].length) {
-          continue;
-        }
-  
-        if(matrix[Y][X] === 1) {
-          matrix[Y][X] = 'x';
-          countSaver.push(count);
-        } else {
-          continue;
-        }
-  
-        queue.push([Y-1, X, count+1]);
-        queue.push([Y+1, X, count+1]);
-        queue.push([Y, X-1, count+1]);
-        queue.push([Y, X+1, count+1]);
+  function bfs(location) {
+    const queue = [location];
+    const countSaver = [];
+    matrix[location[0]][location[1]] = 1;
+
+    while(queue.length > 0) {
+      let [Y, X, count] = queue.shift();
+
+      if(Y < 0 || X < 0 || Y >= matrix.length || X >= matrix[0].length) {
+        continue;
       }
-  
-      return Math.max(...countSaver);
+
+      if(matrix[Y][X] === 1) {
+        matrix[Y][X] = 'x';
+        countSaver.push(count);
+      } else {
+        continue;
+      }
+
+      queue.push([Y-1, X, count+1]);
+      queue.push([Y+1, X, count+1]);
+      queue.push([Y, X-1, count+1]);
+      queue.push([Y, X+1, count+1]);
     }
-  };
-  
+
+    return Math.max(...countSaver);
+  }
+};
