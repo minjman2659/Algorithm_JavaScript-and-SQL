dc05a326cf0fe2574863413df01456fa03ddc4a2
minjman2659 <minjman2659@gmail.com>
2021-08-09 18:37:20 +0900

    Programmers_[Stack]짝지어 제거하기 Solved

--- Programmers/[Stack] 짝지어 제거하기.js
+++ Programmers/[Stack] 짝지어 제거하기.js
@@ -1,8 +1,14 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12973
 
-function solution(s)
-{
-    let stack = [];
+function solution(s) {
+    // s를 반복적으로 돌면서, s[i]와 s[i+1]이 같을 경우 삭제시키는 문제.
+    // 하지만 주의해야할 것이, 짝을 제거했을 시 기존에 있는 알파벳과 짝 제거 후의 알파벳이 같을 경우,
+    // 해당 알파벳 짝 역시 삭제된다는 것이다.
+    // 즉, 반복문을 돌아야하는 원본 값이 계속 변한다는 것이므로,
+    // 원본 대신 짝 제거를 해줄 수 있는 stack 배열을 통해 짝을 제거해 나간다.
+    
+    const stack = [];
+    
     for(let i=0; i<s.length; i++) {
         stack.push(s[i]);
         if(stack[stack.length-2] === stack[stack.length-1]) {
@@ -11,9 +17,5 @@ function solution(s)
         }
     }
     
-    if(stack.length > 0) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return Number(stack.length === 0);
 }
