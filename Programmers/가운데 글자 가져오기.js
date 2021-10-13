f79eeb39273ae2bcb8234f34741483ace59dff1d
minjman2659 <minjman2659@gmail.com>
2021-10-13 19:00:14 +0900

    [Programmers] 가운데 글자 가져오기 Solved

---
+++ Programmers/가운데 글자 가져오기.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12903
+
+function solution(s) {
+  if (s.length % 2 === 0) {
+    return s[s.length / 2 - 1] + s[s.length / 2];
+  } else {
+    return s[Math.floor(s.length / 2)];
+  }
+}
