a4b3fe6ac7fd66d8b702229031616d0321f4b648
minjman2659 <minjman2659@gmail.com>
2021-10-19 23:29:13 +0900

    [SQL Problem] Subquery Problem Solved

---
+++ SQL Problem/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,8 @@
+// https://programmers.co.kr/learn/courses/30/lessons/62284#qna
+
+SELECT DISTINCT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME = 'Milk' AND CART_ID IN (
+    SELECT CART_ID FROM CART_PRODUCTS WHERE NAME = 'Yogurt'
+    )
+ORDER BY ID
