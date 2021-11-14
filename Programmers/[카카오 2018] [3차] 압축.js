6a269d39eb8c0262aff18de53f00e6d84f417220
minjman2659 <minjman2659@gmail.com>
2021-11-14 15:07:43 +0900

    [Programmers] KAKAO2018 압축 Solved

---
+++ Programmers/[카카오 2018] [3차] 압축.js
@@ -0,0 +1,29 @@
+// https://programmers.co.kr/learn/courses/30/lessons/17684
+
+function solution(msg) {
+  const digit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const obj = {};
+  for (let j = 1; j < 27; j++) {
+    obj[digit[j - 1]] = j;
+  }
+
+  let num = 26;
+  const result = [];
+  let i = 0;
+  while (i < msg.length) {
+    let m = i;
+    let tmp = msg[i];
+    while (tmp in obj) {
+      if (!msg[m + 1]) tmp += '!';
+      // msg의 마지막 인덱스일 경우
+      else tmp += msg[++m];
+    }
+    let copied = tmp.split('');
+    copied.pop();
+    i += copied.length;
+    result.push(obj[copied.join('')]);
+    obj[tmp] = ++num;
+  }
+
+  return result;
+}
