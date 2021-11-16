b6eee99c51c63d90fc9dcfa56843757753171a1c
minjman2659 <minjman2659@gmail.com>
2021-11-16 14:17:48 +0900

    [Programmers] DFS/BFS 네트워크 Solved

--- Programmers/[DFS, BFS] 네트워크.js
+++ Programmers/[DFS, BFS] 네트워크.js
@@ -3,40 +3,41 @@
 // 'URclass_자료구조_[BFS/DFS]연결된 정점들' 문제와 비슷
 
 function solution(n, computers) {
-    // 주어진 computers는 인접 행렬
-    let check = new Array(n).fill(false);
-    let count = 0;
-    
-    for(let i=0; i<computers.length; i++) {
-        if(check[i] === false) {
-            dfs(i, computers, check);
-            count++;
-        }
+  let count = 0;
+  const check = new Array(n).fill(false);
+
+  for (let i = 0; i < computers.length; i++) {
+    if (!check[i]) {
+      check[i] = true;
+      bfs(computers[i]);
+      // dfs(computers[i]);
+      count++;
     }
-    
-    // function bfs(from, matrix, check) {
-    //     let queue = [from];
-    //     check[from] = true;
-    //     while(queue.length > 0) {
-    //         let now = queue.shift();
-    //         for(let j=0; j<matrix[now].length; j++) {
-    //             if(matrix[now][j] === 1 && check[j] === false) {
-    //                 check[j] = true;
-    //                 queue.push(j);
-    //             }
-    //         }
-    //     }
-    // }
-    
-    function dfs(from, matrix, check) {
-        check[from] = true;
-        for(let j=0; j<matrix[from].length; j++) {
-            if(matrix[from][j] === 1 && check[j] === false) {
-                let next = j;
-                dfs(next, matrix, check);
-            }
+  }
+
+  return count;
+
+  // bfs
+  function bfs(node) {
+    let queue = [node];
+    while (queue.length > 0) {
+      let now = queue.shift();
+      for (let i = 0; i < now.length; i++) {
+        if (!check[i] && now[i] === 1) {
+          check[i] = true;
+          queue.push(computers[i]);
         }
+      }
     }
-    
-    return count;
+  }
+
+  // dfs
+  function dfs(node) {
+    for (let i = 0; i < node.length; i++) {
+      if (!check[i] && node[i] === 1) {
+        check[i] = true;
+        dfs(computers[i]);
+      }
+    }
+  }
 }
