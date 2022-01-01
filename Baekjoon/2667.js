32ff2ae150e4945035ca50da4694ca28c2db7364
minjman2659 <minjman2659@gmail.com>
2022-01-01 22:48:20 +0900

    [Baekjoon] DFS/BFS Problem Solving

--- Baekjoon/2667.js
+++ Baekjoon/2667.js
@@ -1,46 +1,50 @@
 // https://www.acmicpc.net/problem/2667
 
 function solution(N, matrix) {
-    let home = 0;
-    const homeCount = [];
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
+  let home = 0;
+  const homeCount = [];
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
 
-    const dfs = (x, y) => {
-        if(x >= 0 && x < N && y >= 0 && y < N && matrix[x][y] === 1) {
-            matrix[x][y] = 0;
-            home++;
-            for(let i=0; i<4; i++) {
-                dfs(x+dx[i], y+dy[i]);
-            }
-        }
+  // ------------------------ dfs
+  const dfs = (x, y) => {
+    if (x >= 0 && x < N && y >= 0 && y < N && matrix[x][y] === 1) {
+      matrix[x][y] = 0;
+      home++;
+      for (let i = 0; i < 4; i++) {
+        dfs(x + dx[i], y + dy[i]);
+      }
     }
+  };
 
-    for(let i=0; i<N; i++) {
-        for(let j=0; j<N; j++) {
-            if(matrix[i][j] === 1) {
-                dfs(i, j);
-                homeCount.push(home);
-                home = 0;
-            }
-        }
+  // ------------------------ bfs
+  const bfs = (x, y) => {};
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (matrix[i][j] === 1) {
+        dfs(i, j);
+        homeCount.push(home);
+        home = 0;
+      }
     }
+  }
 
-    homeCount.sort((a, b) => a - b);
-    return homeCount;
+  homeCount.sort((a, b) => a - b);
+  return homeCount;
 }
 
 let matrix = [
-    [0,1,1,0,1,0,0],
-    [0,1,1,0,1,0,1],
-    [1,1,1,0,1,0,1],
-    [0,0,0,0,1,1,1],
-    [0,1,0,0,0,0,0],
-    [0,1,1,1,1,1,0],
-    [0,1,1,1,0,0,0]
+  [0, 1, 1, 0, 1, 0, 0],
+  [0, 1, 1, 0, 1, 0, 1],
+  [1, 1, 1, 0, 1, 0, 1],
+  [0, 0, 0, 0, 1, 1, 1],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0, 0, 0],
 ];
-let sol = solution(7,matrix);
+let sol = solution(7, matrix);
 console.log(sol.length);
-for(let el of sol) {
-    console.log(el);
+for (let el of sol) {
+  console.log(el);
 }
