cf8070b110e25799104fb0a257266d44b5f12926
minjman2659 <minjman2659@gmail.com>
2022-01-02 14:04:58 +0900

    [Baekjoon] BFS/DFS Problem Solved

--- Baekjoon/2667.js
+++ Baekjoon/2667.js
@@ -1,37 +1,59 @@
 // https://www.acmicpc.net/problem/2667
 
+// https://www.acmicpc.net/problem/2667
+
 function solution(N, matrix) {
-  let home = 0;
-  const homeCount = [];
+  const homes = [];
+  let count = 0;
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
 
-  // ------------------------ dfs
-  const dfs = (x, y) => {
-    if (x >= 0 && x < N && y >= 0 && y < N && matrix[x][y] === 1) {
-      matrix[x][y] = 0;
-      home++;
-      for (let i = 0; i < 4; i++) {
-        dfs(x + dx[i], y + dy[i]);
+  const checkBoundary = (x, y) => {
+    if (x >= 0 && x < N && y >= 0 && y < N) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const bfs = (node) => {
+    const queue = [node];
+    while (queue.length > 0) {
+      let [x, y] = queue.shift();
+      if (checkBoundary(x, y) && matrix[x][y] === 1) {
+        matrix[x][y] = 0;
+        count++;
+        for (let i = 0; i < 4; i++) {
+          queue.push([x + dx[i], y + dy[i]]);
+        }
       }
     }
   };
 
-  // ------------------------ bfs
-  const bfs = (x, y) => {};
+  const dfs = ([x, y]) => {
+    if (checkBoundary(x, y) && matrix[x][y] === 1) {
+      matrix[x][y] = 0;
+      count++;
+      for (let i = 0; i < 4; i++) {
+        dfs([x + dx[i], y + dy[i]]);
+      }
+    }
+  };
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
       if (matrix[i][j] === 1) {
-        dfs(i, j);
-        homeCount.push(home);
-        home = 0;
+        bfs([i, j]);
+        dfs([i, j]);
+        homes.push(count);
+        count = 0;
       }
     }
   }
 
-  homeCount.sort((a, b) => a - b);
-  return homeCount;
+  homes.sort((a, b) => a - b);
+  console.log(homes.length);
+  return homes;
 }
 
 let matrix = [
