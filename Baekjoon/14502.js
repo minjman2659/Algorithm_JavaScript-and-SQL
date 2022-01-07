95712fd8185bfae42f538158bbec11045229c75d
minjman2659 <minjman2659@gmail.com>
2022-01-08 01:38:44 +0900

    [Baekjoon] BFS Problem Solving

--- Baekjoon/14502.js
+++ Baekjoon/14502.js
@@ -2,12 +2,19 @@
 
 function solution(N, M, matrix) {
   let result = 0;
-  const matrix2 = matrix.map((el) => el.slice());
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
   let wallCount = 3;
 
-  const bfs = (x, y, check) => {
+  const checkBoundary = (x, y) => {
+    if (x >= 0 && x < N && y >= 0 && y < M) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const bfs = (x, y, check, matrix2) => {
     const queue = [[x, y]];
     check[x][y] = true;
     while (queue.length > 0) {
@@ -15,21 +22,24 @@ function solution(N, M, matrix) {
       for (let i = 0; i < 4; i++) {
         let nx = x + dx[i];
         let ny = y + dy[i];
-        if (!check[nx][ny] && matrix[nx][ny] === 0) {
-          queue.push([nx, ny]);
-          check[nx][ny] = true;
-          matrix2[nx][ny] = 2;
+        if (checkBoundary(nx, ny)) {
+          if (!check[nx][ny] && matrix[nx][ny] === 0) {
+            queue.push([nx, ny]);
+            check[nx][ny] = true;
+            matrix2[nx][ny] = 2;
+          }
         }
       }
     }
   };
 
   const getCount = () => {
+    let matrix2 = matrix.map((el) => el.slice());
     let check = new Array(N).fill(0).map((el) => new Array(M).fill(false));
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
         if (matrix2[i][j] === 2) {
-          bfs(i, j, check);
+          bfs(i, j, check, matrix2);
         }
       }
     }
@@ -38,12 +48,12 @@ function solution(N, M, matrix) {
     tmp.forEach((el) => {
       count += el;
     });
-    return count;
+    result = Math.max(result, count);
   };
 
   const getSolution = (x, y) => {
     if (wallCount === 0) {
-      result = Math.max(result, getCount());
+      getCount();
     }
     for (let i = x; i < N; i++) {
       for (let j = y; j < M; j++) {
@@ -58,5 +68,6 @@ function solution(N, M, matrix) {
     }
   };
 
-  return getSolution(0, 0);
+  getSolution(0, 0);
+  return result;
 }
