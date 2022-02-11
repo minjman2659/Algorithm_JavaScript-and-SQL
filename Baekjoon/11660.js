2d87cc8dc4fabe0e5e35e6a3c1996ff37f7fa51b
minjman2659 <minjman2659@gmail.com>
2022-02-11 13:34:37 +0900

    [Baekjoon] 구간합 Problem Solved

--- Baekjoon/11660.js
+++ Baekjoon/11660.js
@@ -3,9 +3,21 @@
 
 function solution(N, M, matrix, rowColGroup) {
   // rowColGroup은 (x1, y1) ~ (x2, y2)이 담긴 배열
+  const result = [];
+
   for (let arr of rowColGroup) {
-    const copiedMatrix = [...matrix];
     const [x1, y1, x2, y2] = arr;
-    sumMatrix[x1][y1] += matrix[x1][y1];
+    let sum = 0;
+
+    for (let x = x1 - 1; x < x2; x++) {
+      let tempSum = 0;
+      for (let y = y1 - 1; y < y2; y++) {
+        tempSum += matrix[x][y];
+      }
+      sum += tempSum;
+    }
+    result.push(sum);
   }
+
+  return result;
 }
