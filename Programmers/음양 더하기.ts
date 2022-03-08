5a632825936b9f871de963e1ca723480d45d58a4
minjman2659 <minjman2659@gmail.com>
2022-03-08 22:02:50 +0900

    Refact: Typescript 적용

--- Programmers/음양 더하기.ts
+++ Programmers/음양 더하기.ts
@@ -1,7 +1,10 @@
 // https://programmers.co.kr/learn/courses/30/lessons/76501
 
-function solution(absolutes, signs) {
-  let sum = 0;
+function solutionOfAddingYinAndYang(
+  absolutes: number[],
+  signs: boolean[]
+): number {
+  let sum: number = 0;
   for (let i = 0; i < signs.length; i++) {
     if (signs[i] === true) sum += absolutes[i];
     else sum -= absolutes[i];
