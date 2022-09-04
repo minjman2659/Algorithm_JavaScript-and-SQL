923a2ae4191d60e5224fe8d713f8f275e801d467
minjman2659 <minjman2659@gmail.com>
2022-09-04 23:59:12 +0900

    [Programmers] 고득점Kit_폰켓몬 Solved

---
+++ Programmers/고득점 Kit/Hash/폰켓몬.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+function solution(nums) {
+  const possible = nums.length / 2;
+  const ponketmons = new Set();
+  nums.forEach((num) => {
+    ponketmons.add(num);
+  });
+
+  return possible <= ponketmons.size ? possible : ponketmons.size;
+}
