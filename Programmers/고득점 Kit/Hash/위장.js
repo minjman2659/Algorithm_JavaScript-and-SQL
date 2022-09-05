314d16cdc6fcabdfdf43b34a89055a153b39ee0e
minjman2659 <minjman2659@gmail.com>
2022-09-05 22:05:29 +0900

    [Programmers] 고득점Kit_위장 Solved

--- Programmers/고득점 Kit/Hash/위장.js
+++ Programmers/고득점 Kit/Hash/위장.js
@@ -2,18 +2,17 @@
 
 function solution(clothes) {
   let result = 1;
-
   const obj = {};
-  for (let [name, cloth] of clothes) {
+  clothes.forEach(([name, cloth]) => {
     if (cloth in obj) {
       obj[cloth].push(name);
     } else {
       obj[cloth] = [name];
     }
-  }
-
-  for (let key in obj) {
-    result *= obj[key].length + 1;
+  });
+  // 각 옷을 입는 조합의 수 + 각 옷을 안입는 경우
+  for (let cloth in obj) {
+    result *= obj[cloth].length + 1;
   }
   // 아무것도 입지 않는 경우는 없기 때문에 1을 빼주어야 한다.
   return result - 1;
