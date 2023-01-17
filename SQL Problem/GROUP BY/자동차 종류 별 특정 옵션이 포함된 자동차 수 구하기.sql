e9efe5e425b4bebc5cfe7a5ec03b6406832bbaf9
김민재 <81504356+minjman2659@users.noreply.github.com>
2023-01-17 20:39:05 +0900

    [SQL Problem] SQL Problems Solved

---
+++ SQL Problem/GROUP BY/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/151137#qna
+
+SELECT CAR_TYPE, COUNT(CAR_TYPE) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE '%통풍시트%' OR OPTIONS LIKE '%열선시트%' OR OPTIONS LIKE '%가죽시트%'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC
