c701f7e0678d8df10dde43996bd8f7b7425b9f59
minjman2659 <minjman2659@gmail.com>
2023-01-22 21:52:30 +0900

    [SQL Problem] String,Date Problems Solved

---
+++ SQL Problem/String, Date/카테고리 별 상품 개수 구하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131529
+
+SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY, COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY CATEGORY
+ORDER BY CATEGORY ASC
