714b81ff3b04f203bf2a11d54db2925b7f6b39a9
김민재 <81504356+minjman2659@users.noreply.github.com>
2023-03-13 15:25:14 +0900

    [SQL Problem] SELECT Problem Solved

---
+++ SQL Problem/SELECT/조건에 맞는 도서 리스트 출력하기.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144853
+
+SELECT BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+FROM BOOK
+WHERE YEAR(PUBLISHED_DATE) = 2021 AND CATEGORY = '인문'
