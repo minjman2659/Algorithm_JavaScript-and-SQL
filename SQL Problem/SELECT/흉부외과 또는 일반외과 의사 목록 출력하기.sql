75ac39cf76f04ca577d12d91c8b4f8611370785a
김민재 <81504356+minjman2659@users.noreply.github.com>
2023-03-15 16:04:58 +0900

    [SQL Problem] SELECT Problem Solved

---
+++ SQL Problem/SELECT/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132203
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME ASC
