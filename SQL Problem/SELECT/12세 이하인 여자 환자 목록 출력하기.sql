805edcc5a295251e5e8c4735cec47bb63d4ff71f
김민재 <81504356+minjman2659@users.noreply.github.com>
2023-03-15 15:49:18 +0900

    [SQL Problem] SELECT Problems Solved

---
+++ SQL Problem/SELECT/12세 이하인 여자 환자 목록 출력하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132201
+
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE') AS TLNO
+FROM PATIENT
+WHERE AGE <= 12 AND GEND_CD = 'W'
+ORDER BY AGE DESC, PT_NAME ASC
---
+++ SQL Problem/SUM, MAX, MIN/가장 비싼 상품 구하기.sql
@@ -0,0 +1,4 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131697
+
+SELECT MAX(PRICE) AS MAX_PRICE
+FROM PRODUCT
