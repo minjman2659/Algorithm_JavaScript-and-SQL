b5757586d2535c965cbf652df9748fbe6e132aed
minjman2659 <minjman2659@gmail.com>
2023-03-19 15:42:30 +0900

    [SQL Problem] JOIN Problem Solved

---
+++ SQL Problem/JOIN/상품을 구매한 회원 비율 구하기.sql
@@ -0,0 +1,18 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131534
+
+SELECT YEAR(o.SALES_DATE) AS YEAR, MONTH(o.SALES_DATE) AS MONTH,
+       COUNT(DISTINCT o.USER_ID) AS PUCHASED_USERS,
+       ROUND(
+           (COUNT(DISTINCT o.USER_ID)) 
+           / 
+           (SELECT COUNT(USER_ID)
+            FROM USER_INFO 
+            WHERE YEAR(JOINED) = 2021)
+           , 1
+       ) AS PURCHASED_RATIO
+FROM ONLINE_SALE AS o
+LEFT JOIN USER_INFO AS u
+ON o.USER_ID = u.USER_ID
+WHERE YEAR(u.JOINED) = 2021
+GROUP BY YEAR, MONTH
+ORDER BY YEAR ASC, MONTH ASC
