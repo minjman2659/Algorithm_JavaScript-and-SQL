897f7ddc0237c88fec809cfb03f9086a0dff3da6
minjman2659 <minjman2659@gmail.com>
2023-04-15 00:00:48 +0900

    [Programmers] 누적합 Problem Solved

--- Programmers/[누적합] 연속된 부분 수열의 합.js
+++ Programmers/[누적합] 연속된 부분 수열의 합.js
@@ -1,9 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/178870#qna
 
 function solution(sequence, k) {
-  var answer = [];
+  var result = [];
   let prefix = [0]; // sequence의 합을 나타낸 배열
-  let maxL = Infinity; // 정답으로 나온 부분수열의 길이를 나타내는 변수
+  let diffIndex = Infinity; // 정답으로 나온 부분수열의 길이를 나타내는 변수
   sequence.forEach((num, i) => {
     prefix.push(num + prefix[i]);
   });
@@ -21,10 +21,10 @@ function solution(sequence, k) {
     let sum = prefix[right] - prefix[left]; // sequence[left] 부터 sequence[right-1]까지의 수열의 합.
     if (sum === k) {
       // 정답수열을 찾았을때 수열의 길이를 체크하고 가장 작은길이의 수열로 변환.
-      let nowL = right - 1 - left;
-      if (maxL > nowL) {
-        answer = [left, right - 1];
-        maxL = nowL;
+      let temp = right - 1 - left;
+      if (diffIndex > temp) {
+        result = [left, right - 1];
+        diffIndex = temp;
       }
     }
     if (sum < k) {
@@ -32,5 +32,5 @@ function solution(sequence, k) {
     } else left++;
   }
 
-  return answer;
+  return result;
 }
